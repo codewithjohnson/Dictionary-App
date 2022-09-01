d1303f515cf4af2ce7f8906dce7255677db71f55
codewithjohnson <75730906+codewithjohnson@users.noreply.github.com>
2022-09-01 17:08:57 +0100

    adjustments to input containers

--- Dictionary/src/components/Home.jsx
+++ Dictionary/src/components/Home.jsx
@@ -1,20 +1,21 @@
 import React from "react";
-
+import { useState } from "react";
+// <span class="material-symbols-sharp">close</span>
 const Home = () => {
   return (
     <div className="Home">
       <div className="head">
         <h2 className="head__title">English Dictionary</h2>
       </div>
-
-      <div className="input__container">
-        <label htmlFor="input" className="label">
-          Enter a word
-        </label>
-        <input type="text" name="input" />
-        <span class="material-symbols-sharp">close</span>
+      <div className="input__containers">
+        <div className="text__input">
+          <label htmlFor="text" className="label">Enter a word</label>
+          <input type="text" name="text"/>
+        </div>
+        <div className="btn">
+          <button className="submit__btn">search</button>
+        </div>
       </div>
-
       <div className="output_container"></div>
 
       <div className="signature_container">
